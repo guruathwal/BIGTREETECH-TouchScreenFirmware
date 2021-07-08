545713cddc852d3d323a5a4f253304098c081099
guruathwal <gurmeet.athwal@gmail.com>
2021-07-08 16:48:06 +0530

    remove print summary keywords

--- TFT/src/User/API/Printing.c
+++ TFT/src/User/API/Printing.c
@@ -4,14 +4,14 @@
 typedef struct
 {
   FIL        file;
-  uint32_t   time;        // Printed time in sec
-  uint32_t   size;        // Gcode file total size
-  uint32_t   cur;         // Gcode has printed file size
+  uint32_t   time;       // Printed time in sec
+  uint32_t   size;       // Gcode file total size
+  uint32_t   cur;        // Gcode has printed file size
   uint8_t    progress;
-  bool       runout;      // 1: runout in printing, 0: idle
-  bool       printing;    // 1: means printing, 0: means idle
-  bool       pause;       // 1: means paused
-  PAUSE_TYPE pauseType;   // pause type trigged by different sources and gcodes like M0 & M600
+  bool       runout;     // 1: runout in printing, 0: idle
+  bool       printing;   // 1: means printing, 0: means idle
+  bool       pause;      // 1: means paused
+  PAUSE_TYPE pauseType;  // pause type trigged by different sources and gcodes like M0 & M600
 } PRINTING;
 
 PRINTING infoPrinting;
